63cf10f775c3320fae1e3808dcb8e9dafdf85d24
Ritika1311 <ritika.goel@raga.ai>
2025-02-28 10:55:49 +0530

    Add llamaindex instrumentation for more use cases

--- ragaai_catalyst/tracers/utils/extraction_logic_llama_index.py
+++ ragaai_catalyst/tracers/utils/extraction_logic_llama_index.py
@@ -7,7 +7,8 @@ def extract_llama_index_data(data):
     Transform llama_index trace data into standardized format
     """
     data = data[0]
-
+    with open("/Users/ritikagoel/workspace/llamaindex_sub_query_issue/RagaAI-Catalyst/llama_final_result1.json", 'w') as f:
+        json.dump(data, f, indent=4,default=str)        
     # Extract top-level metadata
     trace_data = {
         "project_id": data.get("project_id"),
@@ -25,6 +26,7 @@ def extract_llama_index_data(data):
         }
     }
 
+
     def get_prompt(data):
         for span in data:
             if span["event_type"]=="QueryStartEvent":
@@ -32,6 +34,9 @@ def extract_llama_index_data(data):
                 return prompt
             if span["event_type"]=="QueryEndEvent":
                 prompt = span.get("query", "")
+                return prompt
+            if span["event_type"]=="RetrievalStartEvent":
+                prompt = span.get("str_or_query_bundle", "")
                 return prompt
 
 
@@ -42,10 +47,20 @@ def extract_llama_index_data(data):
                 return context
     
     def get_response(data):
+        responses = []
         for span in data:
-            if span["event_type"]=="QueryEndEvent":
+            if span["event_type"] == "QueryEndEvent":
                 response = span.get("response", "")
-                return response
+                responses.append(response)
+            elif span["event_type"] == "LLMCompletionEndEvent":
+                response = span.get("response", "")
+                responses.append(response)
+        return responses
+            # if span["event_type"]=="LLMCompletionEndEvent":
+            #     response = span.get("response", "")
+            #     return response
+    
+    
             # if span["event_type"]=="LLMPredictEndEvent":
             #     response = span.get("output", "")
             #     return response
@@ -59,9 +74,14 @@ def extract_llama_index_data(data):
                 response = span.get("messages", "")
                 response = response[0]
                 return response
+            if span["event_type"]=="LLMCompletionStartEvent":
+                response = span.get("prompt", "")
+                return response
 
     # Process traces
     if "traces" in data:
+        with open("/Users/ritikagoel/workspace/llamaindex_sub_query_issue/RagaAI-Catalyst/llama_final_result.json", 'w') as f:
+            json.dump(data, f, indent=2,default=str)        
         prompt = get_prompt(data["traces"])
         context = get_context(data["traces"])
         response = get_response(data["traces"])
